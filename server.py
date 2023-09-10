1fda5896a1ddeb1852d3f01bc98894306d55898d
UrizuDev <126583560+urizudev@users.noreply.github.com>
2023-09-10 12:03:53 +0200

    Add files via upload

--- server.py
+++ server.py
@@ -1,41 +1,39 @@
 import threading
 import socket
+import json
+import os
 
-# Now this Host is the IP address of the Server, over which it is running.
-# I've user my localhost.
 host = "localhost"
-port = 5555  # Choose any random port which is not so common (like 80)
+port = 5555
 
 server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-# Bind the server to IP Address
 server.bind((host, port))
-# Start Listening Mode
 server.listen()
-# List to contain the Clients getting connected and nicknames
+
 clients = []
 nicknames = []
 
-
-# 1.Broadcasting Method
 def broadcast(message):
     for client in clients:
         client.send(message)
 
-
-# 2.Receiving Messages from client then broadcasting
 def handle(client):
     while True:
         try:
-            msg = message = client.recv(1024)
-            if msg.decode('ascii').startswith('KICK'):
+            message = client.recv(1024)
+            if not message:
+                remove(client)
+                break
+            msg_str = message.decode('ascii')
+            if msg_str.startswith('KICK'):
                 if nicknames[clients.index(client)] == 'admin':
-                    name_to_kick = msg.decode('ascii')[5:]
+                    name_to_kick = msg_str[5:]
                     kick_user(name_to_kick)
                 else:
                     client.send('Command Refused!'.encode('ascii'))
-            elif msg.decode('ascii').startswith('BAN'):
+            elif msg_str.startswith('BAN'):
                 if nicknames[clients.index(client)] == 'admin':
-                    name_to_ban = msg.decode('ascii')[4:]
+                    name_to_ban = msg_str[4:]
                     kick_user(name_to_ban)
                     with open('bans.txt', 'a') as f:
                         f.write(f'{name_to_ban}\n')
@@ -43,29 +41,27 @@ def handle(client):
                 else:
                     client.send('Command Refused!'.encode('ascii'))
             else:
-                broadcast(message)  # As soon as message received, broadcast it.
+                broadcast(message)
 
         except socket.error:
-            if client in clients:
-                index = clients.index(client)
-                # Index is used to remove client from list after getting disconnected
-                client.remove(client)
-                client.close()
-                nickname = nicknames[index]
-                broadcast(f'{nickname} left the Chat!'.encode('ascii'))
-                nicknames.remove(nickname)
-                break
+            remove(client)
+            break
 
+def remove(client):
+    if client in clients:
+        index = clients.index(client)
+        nickname = nicknames[index]
+        clients.remove(client)
+        client.close()
+        nicknames.remove(nickname)
+        broadcast(f'{nickname} left the Chat'.encode('ascii'))
 
-# Main Receive method
 def receive():
     while True:
         client, address = server.accept()
         print(f"Connected with {str(address)}")
-        # Ask the clients for Nicknames
         client.send('NICK'.encode('ascii'))
         nickname = client.recv(1024).decode('ascii')
-        # If the Client is an Admin prompt for the password.
         with open('bans.txt', 'r') as f:
             bans = f.readlines()
 
@@ -77,7 +73,6 @@ def receive():
         if nickname == 'admin':
             client.send('PASS'.encode('ascii'))
             password = client.recv(1024).decode('ascii')
-            # I know it is lame, but my focus is mainly for Chat system and not a Login System
             if password != 'blockbyeadminLOOL':
                 client.send('REFUSE'.encode('ascii'))
                 client.close()
@@ -90,22 +85,18 @@ def receive():
         broadcast(f'{nickname} joined the Chat'.encode('ascii'))
         client.send('Connected to the Server!'.encode('ascii'))
 
-        # Handling Multiple Clients Simultaneously
         thread = threading.Thread(target=handle, args=(client,))
         thread.start()
-
 
 def kick_user(name):
     if name in nicknames:
         name_index = nicknames.index(name)
         client_to_kick = clients[name_index]
         clients.remove(client_to_kick)
-        client_to_kick.send('You Were Kicked from Chat !'.encode('ascii'))
+        client_to_kick.send('You Were Kicked from Chat!'.encode('ascii'))
         client_to_kick.close()
         nicknames.remove(name)
         broadcast(f'{name} was kicked from the server!'.encode('ascii'))
 
-
-# Calling the main method
 print('Server is Listening ...')
 receive()
